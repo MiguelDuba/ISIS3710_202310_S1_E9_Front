ac0be7ee1292967e50ad81e43851e2ed368d7a3f
mar-cas3 <m.castroi@uniandes.edu.co>
2023-05-05 10:14:46 -0500

    Agregar funciones para formatear precios

---
+++ src/helpers/priceFormatter.js
@@ -0,0 +1,10 @@
+export const formatterUSD = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+export const formatterCOP = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'COP',
+  });
+
