345c0551aee06467dc454223667da91f86b10579
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-05-05 10:57:13 -0500

    Responsive HU_05

--- src/components/usuario/usuarioDetail/UsuarioDetail.js
+++ src/components/usuario/usuarioDetail/UsuarioDetail.js
@@ -1,71 +1,41 @@
-import './UsuarioDetail.css';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row } from "react-bootstrap";
+import "./UsuarioDetail.css"
 
-function UsuarioDetail(props) {
-
-    let antecedentes = <li>No tiene</li>
-    if (props.antecedentes.length > 0) {
-        antecedentes = props.antecedentes.map((antecedente) => {
-            return (
-                <li>{antecedente}</li>
-            );
-        });
-    }
-
-    //Se crea una lista de las habilidades/ del usuario
-    const caracteristicas = props.caracteristicas.map((habilidad) => {
-        return (
-            <li>{habilidad}</li>
-        );
-    });
+function UsuarioDetail() {
 
     return (
-        <section className='detalleUsuario'>
-            <Row>
-                <h1>{props.nombreUsuario}</h1>
-            </Row>
-            <Row className='informacion-general'>
-                <Col>
-                    <p>{props.tipoUsuario}</p>
-                </Col>
-                <Col id="aniosExperiencia">
-                    <p>{props.aniosExperiencia} años de experiencia</p>
-                </Col>                
-            </Row>
-            <Row>
-                <Col className='info-col'>
+        <Row className="detailUsuario">
+            <Col className="no-margin">
+                <Row>
+                    <h1 className="title no-margin">Valerie Robertson</h1> 
+                </Row>
+                <Row>
+                    <Col>Canguro</Col>
+                    <Col>2 años de experiencia</Col>
+                </Row>                
+                <Row>
                     <ul>
                         <lh>Habilidades</lh>
-                        <li>Creatividad </li>
-                        <li>Comunicación efectiva </li>
-                        <li>Certificación en primeros auxilios y RCP </li>
+                        <li>Creatividad</li>
+                        <li>Comunicación efectiva</li>
+                        <li>Certificación en primeros auxilios y RCP</li>
                     </ul>
                     <ul>
                         <lh>Métodos de Contacto</lh>
-                        <li>Celular: {props.celular} </li>
-                        <li>Correo: {props.correoElectronico} </li>
+                        <li>Correo:  v.robertson@gmail.com</li>
+                        <li>Celular:  3012455675</li>
                     </ul>
                     <ul>
                         <lh>Antecedentes</lh>
-                        {antecedentes}
+                        <li>No tiene</li>
                     </ul>
-                </Col>
-                <Col className='centered'>
-                    {/* <img alt="Logo for Facebook"src={props.foto}> */}
-                    <img id="foto" alt="Foto de Perfil" src={props.foto}></img>
-                </Col>
-            </Row>
-            <Row>
-                <Col className='right'>
-                <button type="button">Ver Ofertas</button>
-                </Col>
-                <Col>
-                <button type="button">Ver Reseñas</button>
-                </Col>
-            </Row>
-        </section>
+                </Row>
+            </Col>
+            <Col>
+            
+            </Col>
+        </Row>
     );
 }
-
 
 export default UsuarioDetail;
