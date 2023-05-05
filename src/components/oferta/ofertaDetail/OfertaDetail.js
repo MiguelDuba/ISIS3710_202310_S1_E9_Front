3e60206770ef28d8f935e04b271d47d4a063eea2
mar-cas3 <m.castroi@uniandes.edu.co>
2023-05-05 09:28:48 -0500

    Init version OfertaDetail

--- src/components/oferta/ofertaDetail/OfertaDetail.js
+++ src/components/oferta/ofertaDetail/OfertaDetail.js
@@ -1,31 +1,37 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
+import './OfertaDetail.css';
 
 function OfertaDetail(props) {
     console.log(JSON.stringify(props))
     const info = props.info
+    
     return (
         <Row className='detail'>
+            <Row>
             <Col>
-                <h1>{info.name}</h1>
+                <h1 className="person-name">{info.name}</h1>
                 <Row className="detail--subtitle">
-                    <Col>{info.offerType}</Col>
-                    <Col>{info.experienceNum} años de experiencia</Col>
+                    <Col className='offer-type'>{info.offerType}</Col>
+                    <Col className="experience">{info.experienceNum} años de experiencia</Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <h3>Precio</h3>
-                        <h3>Habilidades</h3>
-                        <h3>Horario</h3>
+                    <Col >
+                        <h3 >Precio</h3>
+                        <h3 >Habilidades</h3>
+                        <h3 >Horario</h3>
                     </Col>
-
-
                 </Row>
             </Col>
+
             <Col>
-            <img src={info.image} alt={`Imagen de ${info.name}`}></img> 
+                <div className="image-cropper">
+                    <img className='rounded person-img'src={info.image} alt={`Imagen de ${info.name}`}></img> 
+                </div>
             </Col>
-            
-            <button>Solicitar</button>
+            </Row>
+            <Row className="row--request">
+                <Button className="btn--request">Solicitar</Button>
+            </Row>
         </Row>
     )
 }
