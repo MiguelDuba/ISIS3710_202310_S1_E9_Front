3abce9e81576ccc0196329d1054fd5cac233cd6a
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-05-06 09:15:52 -0500

    HU_05 Semi terminada Responsive (no props)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Footer from './components/shared/footer/Footer';
 import KangarooNavbar from './components/shared/navbar/KangarooNavbar';
 import UsuarioDetail from './components/usuario/usuarioDetail/UsuarioDetail';
+import UsuarioCreate from './components/usuario/usuarioCreate/usuarioCreate';
 
 function App() {
   return (
     <div className="App">
       <KangarooNavbar/>
       <main>
-        <UsuarioDetail/>
+        <UsuarioCreate/>
       </main>
     </div>
   );
