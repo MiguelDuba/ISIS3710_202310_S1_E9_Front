9bfed615d72d7d022ce89ea5a10519b12a0215ba
mar-cas3 <m.castroi@uniandes.edu.co>
2023-05-09 18:56:02 -0500

    upload data

---
+++ src/tempData/ofertaData.js
@@ -0,0 +1,119 @@
+export const ofertaDetail = {
+    name: "Valerie Robertson", 
+    offerType: "Canguro", 
+    experienceNum: 2, 
+    image: "https://robincollettephotography.com/wp-content/uploads/sites/7154/2021/04/sacramento_headshot_photographer-1-1.jpg",
+    price: 35000, 
+    abilities: ['Creatividad',
+        'Comunicación efectiva',
+        'Certificación en primeros auxilios y RCP'],
+    schedule: [
+        'Martes de 8 am a 10 am', 'Miércoles de 7am a 9am'
+    ]
+}
+
+export const ofertaListData = [
+    {
+      user: {
+        name: "Brooklyn Simmons",
+        offerType: "Canguro",
+        experienceNum: 5,
+        image: "https://image3.photobiz.com/40/6_20200320132439_17431604_xlarge.jpg"
+      },
+      offer: {
+        price: 40000,
+        abilities: [
+          "Creatividad",
+          "Comunicación efectiva",
+          "Certificación en primeros auxilios y RCP",
+        ],
+        schedule: ["Martes de 8 am a 10 am", "Miércoles de 7am a 9am"],
+      },
+    },
+    {
+      user: {
+        name: "Brooklyn Simmons",
+        offerType: "Canguro",
+        experienceNum: 5,
+        image: "https://image3.photobiz.com/40/6_20200320132439_17431604_xlarge.jpg"
+      },
+      offer: {
+        price: 40000,
+        abilities: [
+          "Creatividad",
+          "Comunicación efectiva",
+          "Certificación en primeros auxilios y RCP",
+        ],
+        schedule: ["Martes de 8 am a 10 am", "Miércoles de 7am a 9am"],
+      },
+    },
+    {
+      user: {
+        name: "Brooklyn Simmons",
+        offerType: "Canguro",
+        experienceNum: 5,
+        image: "https://image3.photobiz.com/40/6_20200320132439_17431604_xlarge.jpg"
+      },
+      offer: {
+        price: 40000,
+        abilities: [
+          "Creatividad",
+          "Comunicación efectiva",
+          "Certificación en primeros auxilios y RCP",
+        ],
+        schedule: ["Martes de 8 am a 10 am", "Miércoles de 7am a 9am"],
+      },
+    },
+    {
+      user: {
+        name: "Brooklyn Simmons",
+        offerType: "Canguro",
+        experienceNum: 5,
+        image: "https://image3.photobiz.com/40/6_20200320132439_17431604_xlarge.jpg"
+      },
+      offer: {
+        price: 40000,
+        abilities: [
+          "Creatividad",
+          "Comunicación efectiva",
+          "Certificación en primeros auxilios y RCP",
+        ],
+        schedule: ["Martes de 8 am a 10 am", "Miércoles de 7am a 9am"],
+      },
+    },
+    {
+      user: {
+        name: "Brooklyn Simmons",
+        offerType: "Canguro",
+        experienceNum: 5,
+        image: "https://image3.photobiz.com/40/6_20200320132439_17431604_xlarge.jpg"
+      },
+      offer: {
+        price: 40000,
+        abilities: [
+          "Creatividad",
+          "Comunicación efectiva",
+          "Certificación en primeros auxilios y RCP",
+        ],
+        schedule: ["Martes de 8 am a 10 am", "Miércoles de 7am a 9am"],
+      },
+    },
+    {
+      user: {
+        name: "Brooklyn Simmons",
+        offerType: "Canguro",
+        experienceNum: 5,
+        image: "https://image3.photobiz.com/40/6_20200320132439_17431604_xlarge.jpg"
+      },
+      offer: {
+        price: 40000,
+        abilities: [
+          "Creatividad",
+          "Comunicación efectiva",
+          "Certificación en primeros auxilios y RCP",
+        ],
+        schedule: ["Martes de 8 am a 10 am", "Miércoles de 7am a 9am"],
+      },
+    },
+  ];
+  
