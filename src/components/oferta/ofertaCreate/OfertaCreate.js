59d5a97a9c008b4ac99a79cb53a6b1e0478bae7b
mar-cas3 <m.castroi@uniandes.edu.co>
2023-05-09 08:10:53 -0500

    oferta create

--- src/components/oferta/ofertaCreate/OfertaCreate.js
+++ src/components/oferta/ofertaCreate/OfertaCreate.js
@@ -1,22 +1,58 @@
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import { useState } from 'react';
 import { Button, Col, Form, FormGroup, Row } from "react-bootstrap";
 import CurrencyInput from "react-currency-input-field";
 import { GUARDIAN, KANGAROO, WEEKDAYS } from "../../../helpers/constants";
 import './OfertaCreate.css';
+import { buildOfferPayload, buildSchedulePayload } from './OfertaCreaterHelper';
 
 function OfertaCreate() {
+  let timesByDay = {}
+  WEEKDAYS.forEach((day) => timesByDay[day] = {'start': null, 'end': null})
+  
   const [activeDays, setActiveDays] = useState([])
+  const [activeTimes, setActiveTimes] = useState(timesByDay)
+  const [price, setPrice] = useState(20000)
+  const [offerType, setOfferType] = useState(KANGAROO)
+  const [initDate, setInitDate] = useState(dayjs())
+  const [endDate, setEndDate] = useState(dayjs().add(1, 'd'))
+
 
   const cancelOfferCreate = () => {
     console.log("canceling create...");
   };
 
-  const createOffer = () => {
+  const createOffer = (event) => {
+    event.preventDefault();
     console.log("creating offer...");
+    // validate data
+    const offer = {
+      price: price, 
+      offerType: offerType, 
+      initDate: dayjs(initDate).format('DD/MM/YYYY'), 
+      endDate: dayjs(endDate).format('DD/MM/YYYY'), 
+    }
+    // build payload
+    console.log(buildOfferPayload(offer))
+
+    // post offer
+    console.log('sending post request')
+
+    // assoc schedule
+    const schedulePayloads = []
+    activeDays.forEach((day) => {
+      const dayPayload = buildSchedulePayload(day, activeTimes)
+      schedulePayloads.push(dayPayload)
+    });
+
+    schedulePayloads.forEach( (schedule) => {
+      console.log('sending schedule request for ', schedule)
+    })
+
   };
 
   const showHourForDay = (event) => {
@@ -26,6 +62,15 @@ function OfertaCreate() {
     } else {
       setActiveDays((prevDays) => prevDays.filter((day) => day !== checkedDay))
     }
+  };
+  const updatedActiveTimes = (time, day, start) => {
+    console.log(activeTimes)
+    if(start){
+      activeTimes[day].start = time
+    } else {
+      activeTimes[day].end = time
+    }
+    setActiveTimes(activeTimes)
   };
 
   const renderTime = (day) => {
@@ -37,7 +82,8 @@ function OfertaCreate() {
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Form.Label>Hora Incio</Form.Label>
             <TimePicker
-              value={dayjs('2022-04-17T15:30')}
+              value={activeTimes[day].start}
+              onChange={(newTime) => updatedActiveTimes(newTime, day, true)}
             />
           </LocalizationProvider>
         </Form.Group>
@@ -45,9 +91,10 @@ function OfertaCreate() {
         <Col>
         <Form.Group controlId='form--StartTime'>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Form.Label>Hora Incio</Form.Label>
+            <Form.Label>Hora Fin</Form.Label>
             <TimePicker
-              value={dayjs('2022-04-17T15:30')}
+              value={activeTimes[day].end}
+              onChange={(newTime) => updatedActiveTimes(newTime, day, false)}
             />
           </LocalizationProvider>
         </Form.Group>
@@ -58,10 +105,10 @@ function OfertaCreate() {
   }
 
   return (
-    <Form className='createOffer'>
-        <h1>Nueva Oferta</h1>
+    <Form className="createOffer">
+      <h1>Nueva Oferta</h1>
       <Row>
-        <Col className='form--OfferData'>
+        <Col className="form--OfferData">
           <Form.Group controlId="form--Price">
             <Form.Label>Precio</Form.Label>
             <CurrencyInput
@@ -69,12 +116,16 @@ function OfertaCreate() {
               name="input-price"
               placeholder="Ingresa el precio sin el signo de pesos ($)"
               decimalsLimit={0}
-              prefix={'$'}
+              prefix={"$"}
+              onChange={(e) => setPrice(e.target.value)}
             />
           </Form.Group>
           <FormGroup controlId="form--OfferType">
             <Form.Label>Tipo de Oferta</Form.Label>
-            <Form.Select aria-label="form--OfferType-Input">
+            <Form.Select
+              aria-label="form--OfferType-Input"
+              onChange={(e) => setOfferType(e.target.value)}
+            >
               <option value={KANGAROO}>Canguro</option>
               <option value={GUARDIAN}>Acudiente</option>
             </Form.Select>
@@ -82,41 +133,70 @@ function OfertaCreate() {
           <Row>
             <Col>
               <Form.Group controlId="form--StartDate">
-                <Form.Label>Fecha incial</Form.Label>
-                <Form.Control type="date"></Form.Control>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <Form.Label>Fecha inicial</Form.Label>
+                  <DatePicker
+                    value={initDate}
+                    onChange={(newInitDate) => setInitDate(newInitDate)}
+                  />
+                </LocalizationProvider>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group controlId="form--EndDate">
-                <Form.Label>Fecha final</Form.Label>
-                <Form.Control type="date"></Form.Control>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <Form.Label>Fecha final</Form.Label>
+                  <DatePicker
+                    value={endDate}
+                    onChange={(newEndDate) => setEndDate(newEndDate)}
+                  />
+                </LocalizationProvider>
               </Form.Group>
             </Col>
           </Row>
         </Col>
 
-        <Col className='form--ScheduleData'>
+        <Col className="form--ScheduleData">
           <Form.Group controlId="form--Schedule">
             <Form.Label>Horario</Form.Label>
             {WEEKDAYS.map((day) => {
               return (
                 <Row>
-                  <Form.Check type="checkbox" id={day} value={day} label={day} onClick={showHourForDay}/>
+                  <Form.Check
+                    type="checkbox"
+                    id={day}
+                    value={day}
+                    label={day}
+                    onClick={showHourForDay}
+                  />
                   {renderTime(day)}
                 </Row>
               );
             })}
-
           </Form.Group>
         </Col>
       </Row>
-      
-      <Row className='submitCancelBtns justify-content-md-center'>
-        <Col className='offer-cancelBtn'size={6}>
-        <Button className='offer-cancelBtn'size="lg" type="cancel" onClick={cancelOfferCreate}>Cancelar</Button>
+
+      <Row className="submitCancelBtns justify-content-md-center">
+        <Col className="offer-cancelBtn" size={6}>
+          <Button
+            className="offer-cancelBtn"
+            size="lg"
+            type="cancel"
+            onClick={cancelOfferCreate}
+          >
+            Cancelar
+          </Button>
         </Col>
-        <Col className='offer-createBtn'size={6}>
-        <Button className='offer-createBtn'size="lg" type="submit" onClick={createOffer}>Crear oferta</Button>
+        <Col className="offer-createBtn" size={6}>
+          <Button
+            className="offer-createBtn"
+            size="lg"
+            type="submit"
+            onClick={createOffer}
+          >
+            Crear oferta
+          </Button>
         </Col>
       </Row>
     </Form>
