33b1b6f1983e877cba43220f82fb40332050ee50
Nicolas Rodriguez <na.rodriguezp@uniandes.edu.co>
2023-05-31 18:54:04 -0500

    Ambas partes del landing page terminadas sin internacionalizacion

--- src/components/shared/startpage/startpage-Info/InfoGeneral.js
+++ src/components/shared/startpage/startpage-Info/InfoGeneral.js
@@ -6,6 +6,7 @@ function InfoGeneral(){
             <div class="general--first--text">
                 <h1 style={{ margin: '20px' }}>Uniendo familias y niñeras perfectas junto a Kangaroo</h1>
                 <p>Solución fácil y segura donde familias encuentran niñeras altamente calificadas, mientras que a su vez, niñeras buscan oportunidades de trabajo en hogares que se ajustan a sus habilidades.</p>
+                <button className="btn-t1" type="button">Únete</button>
             </div>
             <div class="general--family--image">
                 <img src="https://cdn.discordapp.com/attachments/260148962948808705/1106280407130980453/Imagen2LandingPage.png" alt="Kangaroo Family"></img>
@@ -18,6 +19,12 @@ function InfoGeneral(){
             <div class="general--second--text">
                 <h3 style={{ margin: '10px' }}>Garantizamos seguridad y transparencia</h3>
                 <p>Kangaroo te brinda tranquilidad con respecto a los usuarios dentro de nuestra plataforma.</p>
+                <ul class="custom-list">
+                    <li>Verificaciones de perfil</li>
+                    <li>Documento de identidad oficial</li>
+                    <li>Constancia Antecedentes Judiciales</li>
+                    <li>Referencias</li>
+                </ul>
             </div>
         </div>
     ];
