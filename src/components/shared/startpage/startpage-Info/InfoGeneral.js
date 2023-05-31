166ba99b33b11dc4558d2d2728cd6774615fbdd8
Nicolas Rodriguez <na.rodriguezp@uniandes.edu.co>
2023-05-31 15:46:58 -0500

    Primera parte landing page funciona

---
+++ src/components/shared/startpage/startpage-Info/InfoGeneral.js
@@ -0,0 +1,17 @@
+import "./InfoGeneral.css"
+
+function InfoGeneral(){
+    return (
+        <div class="general--info--container">
+            <div class="general--first--text">
+                <p>Solución fácil y segura donde familias encuentran niñeras altamente calificadas, mientras que a su vez, niñeras buscan oportunidades de trabajo en hogares que se ajustan a sus habilidades.</p>
+            </div>
+            <div class="general--family--image">
+                <img src="https://cdn.discordapp.com/attachments/260148962948808705/1106280407130980453/Imagen2LandingPage.png" alt="Kangaroo Family"></img>
+            </div>
+        </div>
+
+    );
+}
+
+export default InfoGeneral
