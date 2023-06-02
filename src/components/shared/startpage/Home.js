3dcc85b555af9b8c12e4be722ce194a368db44ec
mar-cas3 <m.castroi@uniandes.edu.co>
2023-06-02 14:25:19 -0500

    Add user create to landing page

--- src/components/shared/startpage/Home.js
+++ src/components/shared/startpage/Home.js
@@ -1,3 +1,4 @@
+import UsuarioCreate from '../../usuario/usuarioCreate/usuarioCreate';
 import InfoAcudiente from "./startpage-Info/InfoAcudiente";
 import InfoCanguro from "./startpage-Info/InfoCanguro";
 import InfoGeneral from "./startpage-Info/InfoGeneral";
@@ -8,6 +9,7 @@ function Home() {
       <InfoGeneral />
       <InfoCanguro />
       <InfoAcudiente />
+      <UsuarioCreate />
     </>
   );
 }
